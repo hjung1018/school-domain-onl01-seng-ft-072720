65351f5977458d891abe17b476f056ebd0de3835
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-07 02:45:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,41 +19,12 @@ class School
   end
   
   def sort
-    @roster[grade].sort
+    @roster.each do |grade, name|
+      grade.sort
   end
+  @roster
 end
 
-
-
-
-#   
-#       @school.add_student("Homer Simpson", 9)
-#       @school.add_student("Jeff Baird", 10)
-#       @school.add_student("Avi Flombaum", 10)
-#       @school.add_student("Blake Johnson", 7)
-
-#       expect(@school.roster).to eq({9 => ["Homer Simpson"], 10 => ["Jeff Baird", "Avi Flombaum"], 7 => ["Blake Johnson"]})
-#     end
-#   end
-
-#   describe '#grade' do
-#     it 'is able to retreive students from a grade' do
-#       @school.add_student("Homer Simpson", 9)
-#       @school.add_student("Avi Flombaum", 10)
-#       @school.add_student("Jeff Baird", 10)
-#       @school.add_student("Blake Johnson", 7)
-
-#       expect(@school.grade(10)).to eq(["Avi Flombaum", "Jeff Baird"])
-#     end
-#   end
-
-#   describe "#sort" do
-#     it 'is able to sort the students' do
-#       @school.add_student("Homer Simpson", 9)
-#       @school.add_student("Bart Simpson", 9)
-#       @school.add_student("Avi Flombaum", 10)
-#       @school.add_student("Jeff Baird", 10)
-#       @school.add_student("Blake Johnson", 7)
 #       @school.add_student("Jack Bauer", 7)
 
 #       # key order does not matter; this is testing that the students in each respective value are in alphabetical order
